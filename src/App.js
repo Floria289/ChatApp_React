fe770bf66e577daf2297c2bf7e704e1480d53932
Floria289 <floria289@hotmail.com>
2023-06-21 18:11:35 -0400

    half

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import { ChatEngine } from "react-chat-engine";
+
+import ChatFeed from './Components/ChatFeed'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+
+
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="ffd31cba-4dd8-40c4-a703-8063ea2c471b"
+            userName="floria289"
+            userSecret="123456"
+            renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps} />}
+        />
+    )
 }
 
 export default App;
